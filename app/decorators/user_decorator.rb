148d568d009cb5e5c34cbf98242cec0809575e93
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-09-05 16:18:28 +0300

    add admin? method to user_decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,5 @@
 class UserDecorator < ApplicationDecorator
-  delegate :id, :full_name, :email
+  delegate :id, :full_name, :email, :admin?
 
   def full_name_with_email
     "#{object.full_name} (#{object.email})"
